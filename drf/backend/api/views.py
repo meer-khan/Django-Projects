55a3f427286cc8d3a5f449e0fa82be45c946d2a1
meer-khan <shahmirkhan519@gmail.com>
2022-08-28 05:17:16 +0500

    28thaugust2022

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from typing import Any
 
 # Create your views here.
+
+def api_home(request, *args, **kwargs):
+
+
+    # we can get the query parameters using the line below 
+    # print(request.GET)
+    body = request.body
+    data = {}
+    # we are using the try catch for the reason that when we dont have the data in the request
+    # which means client doesnot send the data to the url 
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    # print(data.keys())
+    # print (body) # that would be the byte string of json data that client is passing to us
+    # return JsonResponse({'data':'hello this is my data'})
+
+
+
+    # data['headers'] = request.headers
+    # this above line give error of "Headers are not JSON seariable" which means
+    # which means it's not the python dictionary and its could be the datatype of 
+    # <class 'django.http.request.HttpHeaders'> so the JsonResponse method in the return statement 
+    # cannot convert it into the JSON 
+    # So we need to make it the python dictionary before passing it to the JsonReponse method
+
+    # print(type(request.headers))
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    data['params'] = request.GET
+    print(data)
+    return JsonResponse(data)
--- drf/py_client/basics.py
+++ drf/py_client/basics.py
@@ -1,17 +1,17 @@
 import requests
 
 # endpoints = 'https://httpbin.org/anything'
-endpoints = 'http://localhost:8081/'
+endpoints = 'http://localhost:8081/api/'
 
-getResponse = requests.get(endpoints , json={'Data' : 'Hello'})
+getResponse = requests.get(endpoints ,params={'params':'Query Parameters'}, json={'Data' : 'Hello'})
 print (getResponse.json())
-getResponse = requests.get(endpoints , data= {'Data' : 'Hello2'})
-print ('\n',getResponse.json())
-getResponse = requests.get(endpoints)
-print ('\n',getResponse.json())
+# getResponse = requests.get(endpoints , data= {'Data' : 'Hello2'})
+# print ('\n',getResponse.json())
+# getResponse = requests.get(endpoints)
+# print ('\n',getResponse.json()['data'])
 
-# getting status code of the URL response 
+# # getting status code of the URL response 
 
-getResponse = requests.get(endpoints)
-print("*"*50)
-print(getResponse.status_code)
+# getResponse = requests.get(endpoints)
+# print("*"*50)
+# print(getResponse.status_code)
