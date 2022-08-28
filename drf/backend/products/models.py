1b05a77408c8172e3aa1bf27c2da0dbd187933fa
meer-khan <shahmirkhan519@gmail.com>
2022-08-28 05:23:46 +0500

    28thaugust2022 4th commit

--- drf/backend/products/models.py
+++ drf/backend/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=100),
+    content = models.TextField(blank=True , null= True),
+    price = models.DecimalField(default=99.99 , max_digits=15 , decimal_places=2)
