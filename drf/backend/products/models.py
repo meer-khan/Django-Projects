f17698b6e6c844deb5ac9ec0540ccd07924cee35
meer-khan <shahmirkhan519@gmail.com>
2022-08-29 17:58:31 +0500

    trying to establish connection with SQLITE3 but all in vain

--- drf/backend/products/models.py
+++ drf/backend/products/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-
+# from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 class Products(models.Model):
-    title = models.CharField(max_length=100),
+    price = models.DecimalField(default=99.99 , max_digits=15 , decimal_places=2),
+    title = models.CharField(max_length=120),
     content = models.TextField(blank=True , null= True),
-    price = models.DecimalField(default=99.99 , max_digits=15 , decimal_places=2)
+    price = models.DecimalField(default=99.99 , max_digits=15 , decimal_places=2),
+    title = models.CharField(max_length=120),
+    content = models.TextField(blank=True , null= True),
+
+# class Dummy(models.Model):
+#     Name = models.CharField(max_length=255)
+#     kgrams = ArrayField(models.CharField(max_length=255))
+#     cs = models.DecimalField(max_digits=225, decimal_places=0)
+#     hashlist = ArrayField(models.DecimalField(max_digits=225, decimal_places=0))
+#     class Meta:
+#         db_table = 'Dummy'
+
--- drf/py_client/basics.py
+++ drf/py_client/basics.py
@@ -1,10 +1,12 @@
-import requests
+# import requests
 
 # endpoints = 'https://httpbin.org/anything'
 endpoints = 'http://localhost:8081/api/'
 
-getResponse = requests.get(endpoints ,params={'params':'Query Parameters'}, json={'Data' : 'Hello'})
-print (getResponse.json())
+# getResponse = requests.get(endpoints ,params={'params':'Query Parameters'}, json={'Data' : 'Hello'})
+# print (getResponse.json())
+
+
 # getResponse = requests.get(endpoints , data= {'Data' : 'Hello2'})
 # print ('\n',getResponse.json())
 # getResponse = requests.get(endpoints)
@@ -15,3 +17,20 @@ print (getResponse.json())
 # getResponse = requests.get(endpoints)
 # print("*"*50)
 # print(getResponse.status_code)
+
+
+
+
+import re
+value=['[a,b,c]', '[d,e,f]']
+output = []
+for i in value:
+    charactersList = []
+    val = re.sub(",","",i[1:-1])
+    for j in val:
+        charactersList.append(j)
+    output.append(charactersList)
+print(output)
+
+
+
