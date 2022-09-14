4a0c9a55d3a83d17fab3218562201a4c2b6a89fc
Shahmeer <shahmirkhan519@gmail.com>
2022-09-14 16:39:34 +0500

    added ssh key

--- drf/backend/products/tests.py
+++ drf/backend/products/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
 
 # Create your tests here.
+from email import header
+import pandas as pd 
+
+
+file = pd.read_csv(r'C:\Users\Hanzalah\Downloads\jan.csv' , header=None)
+
+# print(file)
+
+user = file.iloc[0]
+
+print(user)
