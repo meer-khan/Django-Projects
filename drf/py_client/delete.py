ba382f43137f063511c6612f8475c3c83c8ac053
Shahmeer Khan <shahmirkhan519@gmail.com>
2022-12-20 23:49:32 +0500

    working

--- drf/py_client/delete.py
+++ drf/py_client/delete.py
@@ -1,10 +1,17 @@
-import requests 
+# import requests 
 
-endpoint= "http://localhost:8080/api/products/10/delete/"
+# endpoint= "http://localhost:8080/api/products/10/delete/"
 
-data = {
-    "title":"this field is completely new",
-    "price" :32.99
-} 
-getResponse = requests.delete(endpoint)
-print(getResponse.status_code)
+# getResponse = requests.delete(endpoint)
+# print(getResponse.status_code)
+
+
+import os 
+import pathlib
+
+
+fullPath = "Hashmap.zip"
+
+ProjectUniqueName = pathlib.Path(fullPath).stem
+
+print(ProjectUniqueName)
