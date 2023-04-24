04291c0899b9cb09bcec2382c7c3b916eecfe9bf
meer-khan <shahmirkhan519@gmail.com>
2023-04-24 17:37:04 +0500

    added a url and sqlite3

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("learnDJ/", views.my_func)
 ]
