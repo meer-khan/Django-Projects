9da2dc45def08924755ebd518bc907bc2d66372a
meer-khan <shahmirkhan519@gmail.com>
2023-04-29 23:59:02 +0500

    apis

--- practice/app1/views.py
+++ practice/app1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+# Basic functional APIs
 # Create your views here.
 def index(request): 
     return HttpResponse("Home Page")
