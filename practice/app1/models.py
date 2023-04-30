26fc562a1bf3867992d3b29100668f9d0be2c17a
meer-khan <shahmirkhan519@gmail.com>
2023-04-30 21:47:27 +0500

    added model

--- practice/app1/models.py
+++ practice/app1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here...
+class Student(models.Model):
+    student_name= models.CharField(max_length=50)
+    student_address = models.CharField(max_length=225)
