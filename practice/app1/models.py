5a2369c4a36098c6f7b009f3d3b5e960d3b1d2a3
meer-khan <shahmirkhan519@gmail.com>
2023-05-01 04:02:33 +0500

    added __str__ method in django

--- practice/app1/models.py
+++ practice/app1/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class Student(models.Model):
     student_name= models.CharField(max_length=50)
     student_address = models.CharField(max_length=225)
+
+# Adding the __str__ function will replace the queryobject in the django admin panel into the value that we use in the return statment
+# Keep in mind that if we are returning the integer value than we need to convert it into string by using str() otherwise
+# django will display an error when we click on the id(record) in django admin panel 
+
+
+    # def __str__(self) -> str:
+    #     return self.student_name
+    
+    # def __str__(self) -> str:
+    #     return self.id
+
+    def __str__(self) -> str:
+        return str(self.id)
