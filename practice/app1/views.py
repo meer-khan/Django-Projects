bcad4223ff99a28ab07b1d221228aed714eeb50f
meer-khan <shahmirkhan519@gmail.com>
2023-05-01 02:47:27 +0500

    get records from db

--- practice/app1/views.py
+++ practice/app1/views.py
@@ -15,5 +15,7 @@ def my_func(request):
 
 def my_func2(request):
     var = Student.objects.all()
+    print(var[0].student_name)
     print(list(var.values()))
-    return HttpResponse(f"HELLO {list(var.values())}")
+    
+    return HttpResponse(f"HELLO {list(var.values())}, NAME: {var[0].student_name}, Address: {var[0].student_address}")
