c93cec49de54a9a96e48ba94d4d26c2b5ca17faa
meer-khan <shahmirkhan519@gmail.com>
2023-05-01 05:42:02 +0500

    ModelAdmin Class with decorator

--- practice/app1/admin.py
+++ practice/app1/admin.py
@@ -11,10 +11,22 @@ from app1.models import Student
 # 2. give all fields name into "list_display" attribute in the form of tuple or list, that we want to display
 # 3. Register ModelAdmin class with the Model class in admin.py
 
+# _________________________CODE1________________________________
+# class StudentAdmin(admin.ModelAdmin):
+#     # "id" is not mentioned in the Model class but we can use it becasue django creates id field in DB if we donot 
+#     # specify any field as primary key.
+#     list_display= ["id","student_name","student_address"]
+
+
+# admin.site.register(Student,StudentAdmin)
+# _________________________END__________________________________
+
+# _________________________CODE2________________________________
+# We can also register our ModelAdmin Class with Model Class for admin using Decorator using below Syntax: 
+# It will work same as above.
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     # "id" is not mentioned in the Model class but we can use it becasue django creates id field in DB if we donot 
     # specify any field as primary key.
     list_display= ["id","student_name","student_address"]
-
-
-admin.site.register(Student,StudentAdmin)
+# _________________________END__________________________________
