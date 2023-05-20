aea5d7005a81c977e6fe7ad77a2ff89a088ceff4
meer-khan <shahmirkhan519@gmail.com>
2023-05-21 02:11:33 +0500

    working on auto_id attribute

--- practice/app1/views.py
+++ practice/app1/views.py
@@ -22,5 +22,27 @@ def my_func2(request):
 
 
 def show_forms_data(request):
+    # BY DEFAULT: auto_id attribute is "id_%s"
     fm = StudentRegistration()
     return render(request,'app1/userregistration.html', {"form":fm})
+
+
+    #* CONFIGURE AUTO_ID
+
+    # # BY DEFAULT: auto_id attribute is "id_%s" 
+    # # if auto_id set to string that includes fromat character %s then form will display id as "whateverstring%s"
+    # fm = StudentRegistration(auto_id="ANYSTRING_%s")
+    # return render(request,'app1/userregistration.html', {"form":fm})
+
+
+    # # if auto_id set to false then form output will not include <label> tag nor id attributes
+    # fm = StudentRegistration(auto_id=False)
+    # return render(request,'app1/userregistration.html', {"form":fm})
+
+    # # if auto_id set to True then form output will include <label> tag and id attributes as name of the field
+    # fm = StudentRegistration(auto_id=True)
+    # return render(request,'app1/userregistration.html', {"form":fm})
+
+    # # if auto_id set to anystring that does not include %s then form will act as auto_id is TRUE
+    # fm = StudentRegistration(auto_id="ANYSTRING")
+    # return render(request,'app1/userregistration.html', {"form":fm})
