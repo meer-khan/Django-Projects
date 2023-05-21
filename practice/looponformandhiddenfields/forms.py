d032f53dbc60b63007f5533e1659c1718a907407
meer-khan <shahmirkhan519@gmail.com>
2023-05-21 21:20:30 +0500

    started working on hidden fields

--- practice/looponformandhiddenfields/forms.py
+++ practice/looponformandhiddenfields/forms.py
@@ -3,4 +3,6 @@ from django import forms
 class StudentRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
-    mobile = forms.CharField()
+    mobile = forms.IntegerField()
+    # Creating a hidden field
+    key = forms.CharField(widget=forms.HiddenInput())
