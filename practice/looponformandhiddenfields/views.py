2074ed18113b574f5c6c41bf143d47854f9a345b
meer-khan <shahmirkhan519@gmail.com>
2023-05-21 20:40:55 +0500

    working on form loops

--- practice/looponformandhiddenfields/views.py
+++ practice/looponformandhiddenfields/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
 # Create your views here.
+
 def forloop(request):
     fm = StudentRegistration()
     # return render(request,'app1/userregistration.html', {"form":fm})
-    return render(request, "looponformandhiddenfields/userregistration.html", {"forms":fm})
+    return render(request, "looponformandhiddenfields/userregistration2.html", {"forms":fm})
