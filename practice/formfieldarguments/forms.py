3d733035eddd5e42c7e3ab603cbe16eb0f769698
meer-khan <shahmirkhan519@gmail.com>
2023-05-26 02:19:05 +0500

    completed common form widgets

--- practice/formfieldarguments/forms.py
+++ practice/formfieldarguments/forms.py
@@ -9,3 +9,11 @@ class StudentRegistration(forms.Form):
     # and values given in the object has higher priority than given in class attributes
     name = forms.CharField(label="Your Name", label_suffix='--',initial="Shahmeer",
                             required=False,disabled=True,help_text="70 Characters")
+    # we can also remove PasswordInput() parenthesis, it will work fine like we did in below field of hiddenInput
+    password = forms.CharField(widget=forms.PasswordInput())
+    hiddenInput = forms.CharField(widget=forms.HiddenInput)
+    textArea = forms.CharField(widget=forms.Textarea)
+    checkbox = forms.CharField(widget=forms.CheckboxInput)
+    fileInput = forms.CharField(widget=forms.FileInput())
+    # Attatching a custom attributes of HTML such as class with any field 
+    email = forms.CharField(widget=forms.EmailInput(attrs={"class": "email", "id":"uniqueID"}))
