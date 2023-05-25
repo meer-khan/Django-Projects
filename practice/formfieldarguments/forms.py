10c327e2ce5bc51e394b13a33149ca8929d24e97
meer-khan <shahmirkhan519@gmail.com>
2023-05-26 01:54:16 +0500

    working on form fields

--- practice/formfieldarguments/forms.py
+++ practice/formfieldarguments/forms.py
@@ -1,4 +1,7 @@
 from django import forms 
 
 class StudentRegistration(forms.Form):
-    name = forms.CharField()
+    # initial argument can be given in forms.py aas well as views.py where we create object of this class
+    # we give intial={} dictionary where we specify inital value of each field as key and value as value
+    name = forms.CharField(label="Your Name", label_suffix='--',initial="Shahmeer",
+                            required=False,disabled=True,help_text="70 Characters")
