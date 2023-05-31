cf2c6da9eddcab9952bef8d7e897c9579aebdeb6
meer-khan <shahmirkhan519@gmail.com>
2023-05-31 23:53:02 +0500

    created a new app for redirecting

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path("formloop/", include("looponformandhiddenfields.urls")),
     path("formfield/",include("formfieldarguments.urls")),
     path("postform/",include("postform.urls")),
-    path("vf/", include("validatingform.urls"))
+    path("vf/", include("validatingform.urls")),
+    path("redirecting/", include("redirecting.urls"))
 
     # path("learnDJ/", app1.my_func),
     # path("myName/", app1.my_func2),
---
+++ practice/redirecting/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import StudentRegistration
+# Create your views here.
+
+def redirecting(request):
+    print(request.body)
+    if request.method == "POST":
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid(): 
+            print("Name: {}".format(fm.cleaned_data["Name"]))
+            print("Email: {}".format(fm.cleaned_data["Email"]))
+            fm = StudentRegistration()
+
+        # print(fm)
+
+    else:
+        fm = StudentRegistration()
+        print(fm)
+        # print("This is Get request")
+    # fm = StudentRegistration()
+    # return render(request,'app1/userregistration.html', {"form":fm})
+    return render(request, "validatingform/userregistration5.html", {"form":fm})
