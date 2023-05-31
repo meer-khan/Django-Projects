1f52f85c2eec9c35aa9059e37c31a456eda4222f
meer-khan <shahmirkhan519@gmail.com>
2023-05-31 08:21:28 +0500

    created view of validatingform

--- practice/validatingform/views.py
+++ practice/validatingform/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import StudentRegistration
 # Create your views here.
 
-def postform(request):
+def validatingform(request):
     fm = StudentRegistration()
     # return render(request,'app1/userregistration.html', {"form":fm})
-    return render(request, "postform/userregistration4.html", {"form":fm})
+    return render(request, "validatingform/userregistration5.html", {"form":fm})
