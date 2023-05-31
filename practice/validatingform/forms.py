2d5dd7cf85d6495ea6b133c61ba10137c25dd1e3
meer-khan <shahmirkhan519@gmail.com>
2023-05-31 08:18:14 +0500

    created a new app named as validating form

---
+++ practice/validatingform/forms.py
@@ -0,0 +1,25 @@
+from django import forms 
+
+class StudentRegistration(forms.Form):
+    # initial argument can be given in forms.py aas well as views.py where we create object of this class
+    # we give intial={} dictionary where we specify inital value of each field as key and value as value
+    # we have done it app1
+    # if we give inital values in both files, forms.py and views.py than what will happen? 
+    # inital value of views.py will display on UI, because in views.py we are creating object of this class
+    # and values given in the object has higher priority than given in class attributes
+    name = forms.CharField()
+    email = forms.EmailField(required=False)
+    # we can also remove PasswordInput() parenthesis, it will work fine like we did in below field of hiddenInput
+
+
+    # password = forms.CharField(widget=forms.PasswordInput())
+    # hiddenInput = forms.CharField(widget=forms.HiddenInput)
+    # textArea = forms.CharField(widget=forms.Textarea)
+    # checkbox = forms.CharField(widget=forms.CheckboxInput)
+    # fileInput = forms.CharField(widget=forms.FileInput())
+
+
+    # Attatching a custom attributes of HTML such as class with any field 
+
+
+    # email = forms.CharField(widget=forms.EmailInput(attrs={"class": "email", "id":"uniqueID"}))
