3e9cad6934b4661ab960ecb28301b5f4d7a9a90b
meer-khan <shahmirkhan519@gmail.com>
2023-06-03 01:26:06 +0500

    worked on all common fields of django forms

--- practice/formfieldtypes10/forms.py
+++ practice/formfieldtypes10/forms.py
@@ -4,9 +4,16 @@ class StudentRegistration(forms.Form):
 
     name = forms.CharField()
     email = forms.EmailField(required=False, min_length=5, max_length=40)
+    roll = forms.IntegerField(min_value=5,max_value=500,)
     price = forms.DecimalField(min_value=5, max_value=40, max_digits=5, decimal_places=2)
     rate = forms.FloatField(min_value=5,max_value=40)
     comment = forms.SlugField()
+    website = forms.URLField( min_length=5,max_length=50,empty_value=None)
+    password = forms.CharField(widget=forms.PasswordInput,min_length=5, max_length=50)   
+    description = forms.CharField(widget=forms.Textarea(attrs={"rows":3,"cols":"50"}),strip=True)
+    feedback = forms.CharField(widget=forms.TextInput(attrs={"class":"Some Class","id":"uniqueID"}), 
+                               min_length=3)
+    agree = forms.BooleanField(label_suffix=" ",label="I Agree", required=True)
 
 
     # we can also remove PasswordInput() parenthesis, it will work fine like we did in below field of hiddenInput
