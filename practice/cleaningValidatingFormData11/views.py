085022aba699ea38bae80f50c112a9ab51f4a943
meer-khan <shahmirkhan519@gmail.com>
2023-06-03 23:52:07 +0500

    working on validations

--- practice/cleaningValidatingFormData11/views.py
+++ practice/cleaningValidatingFormData11/views.py
@@ -3,6 +3,15 @@ from .forms import StudentRegistration
 # Create your views here.
 
 def cleaningValidatingFormData(request):
-    fm = StudentRegistration()
+    if request.method == "POST":
+
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            print("Name is: ", fm.cleaned_data.get("name"))
+            print("Email is: ", fm.cleaned_data["email"])
+    else:
+        fm = StudentRegistration()
+        
     # return render(request,'app1/userregistration.html', {"form":fm})
+
     return render(request, "cleaningValidatingFormData11/userregistration11.html", {"form":fm})
