c4851b30252c8441602744aeb75043e671963cf2
meer-khan <shahmirkhan519@gmail.com>
2023-06-04 02:15:23 +0500

    created a new app for stylign the errors

---
+++ practice/stylingFormErrors12/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .forms import StudentRegistration
+# Create your views here.
+
+def stylingFormErrors(request):
+    if request.method == "POST":
+
+        fm = StudentRegistration(request.POST)
+        print("THIS IS FM", fm)
+        # This try block is for email specifically, if we write email without @ or not with email format 
+        # than Django server gives error that NoneType object has no method len()
+        # Try except block catch the exception and than Django automatically prompts on webUI that 
+        # Email should be in correct format 
+        # This is not taught in lectures, I have explored it on my own 
+        # try:
+        if fm.is_valid():
+            print("Name is: ", fm.cleaned_data.get("name"))
+            print("Email is: ", fm.cleaned_data["email"])
+        # except Exception as e: 
+        #     print(e) 
+    else:
+        fm = StudentRegistration()
+    
+    # fm = StudentRegistration()
+    # return render(request,'app1/userregistration.html', {"form":fm})
+
+    return render(request, "stylingFormErrors12/userregistration12.html", {"form":fm})
