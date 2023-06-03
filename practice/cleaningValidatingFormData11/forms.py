c943361e64fbcd288467e308783facfd49d42ad0
meer-khan <shahmirkhan519@gmail.com>
2023-06-04 01:54:14 +0500

    done password and confirm password validation

--- practice/cleaningValidatingFormData11/forms.py
+++ practice/cleaningValidatingFormData11/forms.py
@@ -21,15 +21,16 @@ class StudentRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField(required=False)
     password = forms.CharField(widget=forms.PasswordInput())
-
+    # We are creating this field to reenter password and makes sure, both password match by using 
+    # the clean() function, in clean function we write custom logic to check both passwords are same or not
+    repassword = forms.CharField(widget=forms.PasswordInput(), label="Confirm Password")
     
     # * Writing Builtin validators: 
     '''
     CUSTOM VALIDATION FIELDS
     '''
     fullName = forms.CharField(validators=[validators.MaxLengthValidator(20), 
-                                           validators.MinLengthValidator(5),
-                                            ])
+                                           validators.MinLengthValidator(5),])
     
     pho_no = forms.CharField(validators=[phone_num_starts_with_92])
 
@@ -60,16 +61,26 @@ class StudentRegistration(forms.Form):
         '''
         This function is to validate the complete form
         '''
-        # cleaned_data = super().clean()
+
+        # we write below line to ensure that clean() class of django's validations also perfomed in our StudentRegistration Class
+        # TODO: But code works without creating this object, So, it's little unclear concept right now
+        cleaned_data = super().clean()
         valName = self.cleaned_data["name"]
         valEmail = self.cleaned_data.get("email")
-        valNo = self.cleaned_data["ph_no"]
+        valNo = self.cleaned_data["pho_no"]
+        valpass = self.cleaned_data["password"]
+        revalpass = self.cleaned_data["repassword"]
+        if revalpass != valpass:
+            raise forms.ValidationError("Passwords does not match")
 
         if len(valName) < 4 : 
             raise forms.ValidationError("Enter Name with more than 4 character and make sure there is no special character in it")
         
         if len(valEmail)< 10 :
             raise forms.ValidationError("Email should not include less than 10 characters")
+        
+
+        
         # return valName
 
 
--- practice/cleaningValidatingFormData11/views.py
+++ practice/cleaningValidatingFormData11/views.py
@@ -6,18 +6,18 @@ def cleaningValidatingFormData(request):
     if request.method == "POST":
 
         fm = StudentRegistration(request.POST)
-
+        print("THIS IS FM", fm)
         # This try block is for email specifically, if we write email without @ or not with email format 
         # than Django server gives error that NoneType object has no method len()
         # Try except block catch the exception and than Django automatically prompts on webUI that 
         # Email should be in correct format 
         # This is not taught in lectures, I have explored it on my own 
-        try:
-            if fm.is_valid():
-                print("Name is: ", fm.cleaned_data.get("name"))
-                print("Email is: ", fm.cleaned_data["email"])
-        except Exception as e: 
-            print(e) 
+        # try:
+        if fm.is_valid():
+            print("Name is: ", fm.cleaned_data.get("name"))
+            print("Email is: ", fm.cleaned_data["email"])
+        # except Exception as e: 
+        #     print(e) 
     else:
         fm = StudentRegistration()
     
