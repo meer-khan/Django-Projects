39670611e3701a3bbab81da62ffd67fcd3a499d3
meer-khan <shahmirkhan519@gmail.com>
2023-06-07 23:56:48 +0500

    typo

--- practice/stylingFormErrors12/forms.py
+++ practice/stylingFormErrors12/forms.py
@@ -19,7 +19,7 @@ from django.core import validators
 class StudentRegistration(forms.Form):
 
     # If Name field is empty than "Enter Your Name" error will pop up
-    name = forms.CharField(error_messages={"required":"Enter Your Name"}, min_length=5,max_length=50)
+    name = forms.CharField(error_messages={"required":"Enter Your Name: "}, min_length=5,max_length=50)
     email = forms.EmailField(error_messages={"required":"Enter Your Email"})
     password = forms.CharField(widget=forms.PasswordInput(),error_messages={"required":"Enter Your Password"})
     # # We are creating this field to reenter password and makes sure, both password match by using 
