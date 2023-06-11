7852f45dab81ef5f389cf1140fb721913951d422
meer-khan <shahmirkhan519@gmail.com>
2023-06-11 15:03:01 +0500

    applied migrations

--- practice/modelForms13/models.py
+++ practice/modelForms13/models.py
@@ -5,3 +5,14 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
+
+    # Setting Name of Model (Table)
+    # 1 - we can create a db_table attribute in models.Model inherited class
+    # 2 - Create a Meta class and than create db_table 
+
+    # Let's rename Table with first approach 
+    # db_table = "user_registration"
+    # ! First approach does not work, Bard is SHITTTTT!!!!!
+
+    class Meta:
+        db_table = "user_registration"
