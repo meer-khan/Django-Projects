c5b075a54ac5a1a6023fab403d83d35ce657764c
meer-khan <shahmirkhan519@gmail.com>
2023-06-11 16:39:45 +0500

    created a form using model forms

--- practice/modelForms13/forms.py
+++ practice/modelForms13/forms.py
@@ -6,11 +6,14 @@ from .models import User
 class StudentRegistration(forms.ModelForm):
     class Meta: 
         model = User
+        # WE CAN REORDER THE FIELDS BY REORDERING THEM IN BELOW LIST
         fields = ["name", "email", "password"]
-    name = forms.CharField(error_messages={"required":"Enter Your Name"}, min_length=5,max_length=50)
-    email = forms.EmailField(error_messages={"required":"Enter Your Email"})
-    password = forms.CharField(widget=forms.PasswordInput(),error_messages={"required":"Enter Your Password"})
-
+        labels = {"name": "Enter Name", "email": "Enter Email", "password": "Enter Pass"}
+        help_text = {"name": "Enter Full Name", "email": "Enter Official Email", "password": "Enter Strong Password"}
+        error_messages = {"name": {"required": "Name is cumpolsory"}}
+        widgets = {"password": forms.PasswordInput(attrs={"placeholder":"Strong Password"}), 
+                   "name": forms.TextInput(attrs={"class":"nameClass", "placeholder":"Full Name"}),
+                   "email":forms.TextInput(attrs={"placeholder":"Email"})}
 
 
 
--- practice/modelForms13/models.py
+++ practice/modelForms13/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    # if we give argument blank = True, then field will not longer be required in the form, and 
+    # required argument of field in forms.py will automatically be set to false
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
