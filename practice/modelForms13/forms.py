73c9980a5c2d9f0c4df5102ae9945167d2f716a7
meer-khan <shahmirkhan519@gmail.com>
2023-06-11 14:21:43 +0500

    created a first model form

--- practice/modelForms13/forms.py
+++ practice/modelForms13/forms.py
@@ -1,22 +1,12 @@
 from django import forms 
 import re
 from django.core import validators
+from .models import User
 
-
-
-
-# def phone_num_starts_with_92(phno):
-#     '''
-#     This function is a custom validator
-#     '''
-#     # if phno[0] != '+':
-#     #     raise forms.ValidationError("Number should start from +")
-#     if not phno.startswith("+92"):
-#         raise forms.ValidationError("Number should start from +92")
-
-    
-
-class StudentRegistration(forms.Form):
+class StudentRegistration(forms.ModelForm):
+    class Meta: 
+        model = User
+        fields = ["name", "email", "password"]
     name = forms.CharField(error_messages={"required":"Enter Your Name"}, min_length=5,max_length=50)
     email = forms.EmailField(error_messages={"required":"Enter Your Email"})
     password = forms.CharField(widget=forms.PasswordInput(),error_messages={"required":"Enter Your Password"})
--- practice/modelForms13/views.py
+++ practice/modelForms13/views.py
@@ -7,21 +7,16 @@ def modelForms(request):
 
         fm = StudentRegistration(request.POST)
         print("THIS IS FM", fm)
-        # This try block is for email specifically, if we write email without @ or not with email format 
-        # than Django server gives error that NoneType object has no method len()
-        # Try except block catch the exception and than Django automatically prompts on webUI that 
-        # Email should be in correct format 
-        # This is not taught in lectures, I have explored it on my own 
-        # try:
         if fm.is_valid():
+            nm = fm.cleaned_data["name"]
+            em = fm.cleaned_data["email"]
+            ps = fm.cleaned_data["password"]
             print("Name is: ", fm.cleaned_data.get("name"))
             print("Email is: ", fm.cleaned_data["email"])
-        # except Exception as e: 
-        #     print(e) 
+            print("Password is: ", fm.cleaned_data["password"])
+
+        
     else:
         fm = StudentRegistration()
-    
-    # fm = StudentRegistration()
-    # return render(request,'app1/userregistration.html', {"form":fm})
 
     return render(request, "modelForms13/userregistration13.html", {"form":fm})
