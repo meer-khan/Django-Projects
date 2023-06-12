66df50687222e2434f4d10cbd35b2a076e725a4e
meer-khan <shahmirkhan519@gmail.com>
2023-06-13 01:33:37 +0500

    created a dynamic url with default path converter

--- practice/dynamicURL14/views.py
+++ practice/dynamicURL14/views.py
@@ -3,23 +3,7 @@ from .forms import StudentRegistration
 from .models import User
 # Create your views here.
 
-def dynamicURL(request):
-    if request.method == "POST":
+def dynamicURL(request,id):
+    
 
-        fm = StudentRegistration(request.POST)
-        print("THIS IS FM", fm)
-        if fm.is_valid():
-            nm = fm.cleaned_data["name"]
-            em = fm.cleaned_data["email"]
-            ps = fm.cleaned_data["password"]
-            print("Name is: ", fm.cleaned_data.get("name"))
-            print("Email is: ", fm.cleaned_data["email"])
-            print("Password is: ", fm.cleaned_data["password"])
-            obj = User(name = nm, email = em, password = ps)
-            obj.save()
-
-        
-    else:
-        fm = StudentRegistration()
-
-    return render(request, "dynamicURL14/userregistration14.html", {"form":fm})
+    return render(request, "dynamicURL14/userregistration14.html",{"id":id})
