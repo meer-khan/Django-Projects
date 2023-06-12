6c6f033af397fdafa0f68f14c19e2fb90f25b883
meer-khan <shahmirkhan519@gmail.com>
2023-06-13 01:51:54 +0500

    created a new html file

--- practice/dynamicURL14/urls.py
+++ practice/dynamicURL14/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
+    # path converter is not set (no data type attached), so "id" is considered as string
     path("student/<id>", views.dynamicURL, name="details" )
 ]
