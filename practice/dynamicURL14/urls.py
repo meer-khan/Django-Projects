12b287738ac9b212175a9f15161a0d042447e9d2
meer-khan <shahmirkhan519@gmail.com>
2023-06-13 04:03:25 +0500

    created path converters dynamic urls

--- practice/dynamicURL14/urls.py
+++ practice/dynamicURL14/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     # path converter is not set (no data type attached), so "id" is considered as string
-    path("student/<id>", views.dynamicURL, name="details" )
+    path("student/<id>", views.dynamicURL, name="detail" ),
+    path("home/", views.home, name = "home"),
+    path("", views.home, name = "home"),
+    path("details/<int:id>", views.integerID, name = "userDetails"),
+    path("details/<int:id>/<int:subid>", views.integerID, name = "subDetails"),
+
+
+
 ]
