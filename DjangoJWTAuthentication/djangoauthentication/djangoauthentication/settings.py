c99beb6a433a5b473b0ec964f5d12d158f93472b
meer-khan <shahmirkhan519@gmail.com>
2023-06-15 01:05:15 +0500

    applied new migrations and connected the db.sqlite3 database

--- DjangoJWTAuthentication/djangoauthentication/djangoauthentication/settings.py
+++ DjangoJWTAuthentication/djangoauthentication/djangoauthentication/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = "djangoauthentication.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     # "default": {
-#     #     "ENGINE": "django.db.backends.sqlite3",
-#     #     "NAME": BASE_DIR / "db.sqlite3",
-#     # }
-
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TestDB',
-        'USER': 'postgres',
-        'PASSWORD': 'Pakistan2212',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'TestDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Pakistan2212',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
