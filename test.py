4344ee6ce8fe8f7bf5323bd6163741b156110488
Shahmeer <shahmirkhan519@gmail.com>
2023-06-19 19:59:38 +0500

    longest substring without repeating characters

--- test.py
+++ test.py
@@ -18,24 +18,50 @@
 #         return totalCount
 
 
-def lengthOfLongestSubstring(s: str) -> int:
+# def lengthOfLongestSubstring(s: str) -> int:
+#         chars = []
+#         count = 0
+#         totalCount = 0
+#         matchStart = 0
+#         for char in range(len(s)): 
+#             if s[char] not in chars: 
+#                 # if matchStart == char
+#                 # matchStart = char
+#                 chars.append(s[char])
+#                 count+=1
+#                 if totalCount < count: 
+#                      totalCount = count
+#             else:  
+#                 # totalCount = len(chars)
+#                 # chars.remove(s[char])
+#                 for i in range (char,len(chars)):
+#                      chars.pop(i) 
+#                 # count = 1
+#                 chars.append(s[char])
+
+
+
+def lengthOfLongestSubstring( s: str) -> int:
         chars = []
         count = 0
-        totalCount = 0
-        for char in range(len(s)): 
-            if s[char] not in chars: 
-                chars.append(s[char])
-                count+=1
-                if totalCount < count: 
-                     totalCount = count
-            else:  
-                # totalCount = len(chars)
-                chars.remove(s[char])
-                # count = 1
-                chars.append(s[char])
-                
-                
-        return totalCount,chars
+        totalCount = 1
+        matchStart = 0
+        if len(s) == 0:
+            return 0
+        for iIndex in range(len(s)):
+            if s[iIndex] not in chars:
+                chars.append(s[iIndex]) 
+            for jIndex in range(iIndex+1,len(s)): 
+                if s[jIndex] not in chars: 
+                        chars.append(s[jIndex])
+                        count = len(chars)
+                else:  
+                    chars = []
+                    break
+            
+            if totalCount < count:
+                 totalCount = count
+        return totalCount
 
 
 
