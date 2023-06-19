80050c5c7e7060eec7661b2d2b72efc4372d4d7c
Shahmeer <shahmirkhan519@gmail.com>
2023-06-19 17:06:52 +0500

    created url of password reset

--- DjangoJWTAuthentication/djangoauthentication/account/urls.py
+++ DjangoJWTAuthentication/djangoauthentication/account/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from account.views import UserRegistrationView,UserLoginView,UserProfileView,UserChangePasswordView,SendPasswordResetEmailView
+from account.views import (UserRegistrationView,UserLoginView,UserProfileView,UserChangePasswordView,
+                           SendPasswordResetEmailView,UserPasswordResetView)
 urlpatterns = [
 
     path("register/",UserRegistrationView.as_view(), name="register" ),
     path("login/",UserLoginView.as_view(), name="login" ),
     path('profile/',UserProfileView.as_view(), name='profile'),
     path('changepassword/',UserChangePasswordView.as_view(), name='changepassword'),
-    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name= 'resetpassword')
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name= 'resetpassword'), 
+    path('reset-password/<uid>/<token>', UserPasswordResetView.as_view(), name = 'reset-password'),
 
 
 ]
