ccad0347238ffa4942c340e60c56ba735d2ec6ea
meer-khan <shahmirkhan519@gmail.com>
2023-06-28 18:07:24 +0500

    registered the model and ran migrations

--- djangoCRUDOps/djangoCRUDOps/settings.py
+++ djangoCRUDOps/djangoCRUDOps/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = "djangoCRUDOps.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DjangoCrudProject',
+        'USER': 'postgres',
+        'PASSWORD': 'Pakistan2212',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
