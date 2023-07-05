09f41669ddf7e90d7a4cfc881465042a52ac2125
meer-khan <shahmirkhan519@gmail.com>
2023-07-06 03:52:46 +0500

    created delete functionality

--- djangoCRUDOps/enroll/urls.py
+++ djangoCRUDOps/enroll/urls.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('addnshow/', views.add_show, name='addnshow')
+    path('', views.add_show, name='addnshow'), 
+
+    path('addnshow/', views.add_show, name='addnshow'), 
+    path("delete/<int:id>" , views.delete_data, name="delete")
 ]
--- djangoCRUDOps/enroll/views.py
+++ djangoCRUDOps/enroll/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .forms import StudentRegistration
 from .models import User
 # Create your views here.
@@ -51,3 +51,16 @@ def add_show(request):
 #         fm = StudentRegistration()
 
 #     return render(request, 'enroll/addandshow.html', {'form':fm})
+
+
+
+
+# Delete Function
+
+
+def delete_data(request,id):
+    if request.method == "POST": 
+        record = User.objects.get(pk=id)
+        record.delete()
+
+    return HttpResponseRedirect('/')
