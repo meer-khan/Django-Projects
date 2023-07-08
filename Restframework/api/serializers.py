1e0d5c9036debc994a986bdc28e333ec9441b28d
meer-khan <shahmirkhan519@gmail.com>
2023-07-09 03:11:53 +0500

    attached Id as well in serializer

--- Restframework/api/serializers.py
+++ Restframework/api/serializers.py
@@ -3,6 +3,7 @@ from .models import Student
 
 
 class StudentSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     roll = serializers.IntegerField()
     city = serializers.CharField(max_length=100)
