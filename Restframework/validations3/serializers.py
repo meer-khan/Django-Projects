8176720cdf49c1e283b75d2fd630ad9a33f41cfd
meer-khan <shahmirkhan519@gmail.com>
2023-07-12 03:45:14 +0500

    updated serializer with validation

--- Restframework/validations3/serializers.py
+++ Restframework/validations3/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from serialization.models import Student
 
 
-
+def city_validation(data):
+    if data == 'MBDIN':
+        raise serializers.ValidationError("no city available")
+    return data
 class StudentSerializer(serializers.Serializer):
     # id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
+    city = serializers.CharField(max_length=100, validators = [city_validation])
 
     def create(self,validated_data):
         return Student.objects.create(**validated_data)
@@ -22,4 +25,26 @@ class StudentSerializer(serializers.Serializer):
         instance.save()
         return instance
 
+
+
+    # FIRST DJNAGO WILL EXECUTE FIELD LEVEL VALIDATORS AND THEN OBJECT LEVEL VALIDATIONS
+
+
+    # Field Level Validation
+    def validate_roll(self,value):
+        if value >= 200:
+            raise serializers.ValidationError('Seats Full')
+        return value
+    
+
+    # Object Level Validation
+    def validate(self,data):
+        name = data.get('name')
+        city = data.get('city')
+        if data.get('roll') == 200:
+            raise serializers.ValidationError("Ohh bhai kia kar raha hai tu?")
+        if name.lower() == "kashif" and city != "MBDIN":
+            raise serializers.ValidationError(f"Ohoo {name} should be from MBDIN")
+        
+        return data
 
--- Restframework/validations3/urls.py
+++ Restframework/validations3/urls.py
@@ -3,11 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # URL 1 -- POSTMAN
-    path("studentinfo/",view=views.student_detail),
-
-    # URL 2 -- POSTMAN
-    path("studentlist/",view=views.student_list), 
     path("validataion/", view = views.StudentAPI.as_view())
 
 ]
