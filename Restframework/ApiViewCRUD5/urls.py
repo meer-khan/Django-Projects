c3f755007be2b41f498a7e396eb812f6035f336d
meer-khan <shahmirkhan519@gmail.com>
2023-07-15 23:25:19 +0500

    updated the url for class based api view

--- Restframework/ApiViewCRUD5/urls.py
+++ Restframework/ApiViewCRUD5/urls.py
@@ -3,8 +3,16 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("ApiViewCRUD/", view = views.student_api),
-    path("ApiViewCRUD/<int:id>", view = views.student_api)
+    # *Function Based APIs
+    # path("ApiViewCRUD/", view = views.student_api),
+    # path("ApiViewCRUD/<int:id>", view = views.student_api),
+
+    # *Testing Class based views: 
+    path("ApiViewCRUD/", view = views.StudentAPI.as_view()),
+    path("ApiViewCRUD/<int:id>", views.StudentAPI.as_view()),
+
+
+    
 
 
 ]
