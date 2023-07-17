ec5339ec1e692d023cc99feb773affd0477e6d80
meer-khan <shahmirkhan519@gmail.com>
2023-07-17 16:04:59 +0500

    created a same class for list and create model mixins

--- Restframework/GenericAPIandMixin6/views.py
+++ Restframework/GenericAPIandMixin6/views.py
@@ -1,7 +1,7 @@
 from serialization.models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 # Create your views here.
 
 class StudentList(GenericAPIView, ListModelMixin):
@@ -33,4 +33,37 @@ class StudentUpdate(GenericAPIView, UpdateModelMixin):
     serializer_class = StudentSerializer
     def put(self,request,*args,**kwargs):
         return self.update(request,*args,**kwargs)
+    
+
+
+class StudentDestroy(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
+    
+
+
+# * IMPORTANT: So we need the "pk" paramter in the Retrieve, Update, and Destroy APIs 
+# * and we donot need "pk" in the List and Create APIs. 
+# * So, we can group these APIs and then we need to create only 2 URLs 
+
+# * Let's do it here. 
+# * For this purpose, we will create a new class of LC, StudentAPI, and it will be inherited by "ListModelMixin", and "CreateModelMixin"
+# * LIKE BELOW: and will create get and post methods in same class
+# * After that we can create a single URL of it
+
+class LCStudentAPI(GenericAPIView, ListModelMixin,CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+
+
+
+
     
