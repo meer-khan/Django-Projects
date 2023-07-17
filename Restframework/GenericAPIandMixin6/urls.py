8d8c50ac50d022627887572a1ede2aa494e3f54e
meer-khan <shahmirkhan519@gmail.com>
2023-07-17 13:24:13 +0500

    created a POST ModelMixin

--- Restframework/GenericAPIandMixin6/urls.py
+++ Restframework/GenericAPIandMixin6/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     # *Generic Mixin Classes
-    path("GenericAPIandMixin/", view = views.StudentList.as_view()),
+    path("GenericAPIandMixin/", view = views.StudentCreate.as_view()),
+    # path("GenericAPIandMixin/", view = views.StudentList.as_view()),
+
     # path("GenericAPIandMixin/<int:id>", views.StudentAPI.as_view()),
 
 
--- Restframework/GenericAPIandMixin6/views.py
+++ Restframework/GenericAPIandMixin6/views.py
@@ -1,7 +1,7 @@
 from serialization.models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 # Create your views here.
 
 class StudentList(GenericAPIView, ListModelMixin):
@@ -10,4 +10,11 @@ class StudentList(GenericAPIView, ListModelMixin):
     def get(self,request,*args,**kwargs):
         return self.list(request,*args,**kwargs)
     
+
+
+class StudentCreate(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
     
