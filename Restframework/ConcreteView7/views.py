3dc9eb44054b4973916f7c71a1bc0a3c5925f87f
meer-khan <shahmirkhan519@gmail.com>
2023-07-19 00:14:06 +0500

    wrote a crreate api

--- Restframework/ConcreteView7/views.py
+++ Restframework/ConcreteView7/views.py
@@ -1,8 +1,13 @@
 from serialization.models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 # Create your views here.
 
 class StudentList(ListAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+
+
+class StudentCreate(CreateAPIView):
   queryset = Student.objects.all()
   serializer_class = StudentSerializer
