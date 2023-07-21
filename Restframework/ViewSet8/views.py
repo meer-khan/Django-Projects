d582fce49d367da523804b88b6ff18f73473a085
meer-khan <shahmirkhan519@gmail.com>
2023-07-22 00:19:44 +0500

    created a viewset list api

--- Restframework/ViewSet8/views.py
+++ Restframework/ViewSet8/views.py
@@ -1,75 +1,13 @@
 from serialization.models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import (ListAPIView, CreateAPIView, 
-                                     RetrieveAPIView, UpdateAPIView, 
-                                     DestroyAPIView, ListCreateAPIView,
-                                     RetrieveUpdateAPIView,
-                                     RetrieveDestroyAPIView,
-                                     RetrieveUpdateDestroyAPIView)
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+
 # Create your views here.
 
-class StudentList(ListAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-class StudentCreate(CreateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-
-class StudentRetrieve(RetrieveAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-class StudentUpdate(UpdateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-class StudentDestroy(DestroyAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-
-
-
-
-class StudentListCreate(ListCreateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-class StudentRetrieveUpdate(RetrieveUpdateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-class StudentRetrieveDestroy(RetrieveDestroyAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-class StudentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-
-
-
-
-
-# GROUP CLASSES that will perform all operations: 
-class StudentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-class StudentListCreate(ListCreateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-
-
-
+class StudentViewSet(viewsets.ViewSet):
+    def list(self,request):
+      stu = Student.objects.all()
+      serializer = StudentSerializer(stu,many=True)
+      return Response(serializer.data)
