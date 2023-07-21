de9aa978b897a27f219c7a156954aa2ac8f658ca
meer-khan <shahmirkhan519@gmail.com>
2023-07-22 00:25:37 +0500

    created more apis using viewset

--- Restframework/ViewSet8/views.py
+++ Restframework/ViewSet8/views.py
@@ -11,3 +11,28 @@ class StudentViewSet(viewsets.ViewSet):
       stu = Student.objects.all()
       serializer = StudentSerializer(stu,many=True)
       return Response(serializer.data)
+    
+    def retrieve(self,request, pk=None):
+      if pk is not None: 
+         
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu)
+        return Response(serializer.data)
+      
+
+    def create(self,request):
+      serializer = StudentSerializer(data = request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+      return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    
+    def update(self,request,pk):
+      # id = pk 
+      if pk is not None:
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu)
+        if serializer.is_valid():
+          serializer.save()
+          return Response({"msg":"Record Updated"})
