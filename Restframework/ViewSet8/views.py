93ce1f4efceebeadec3b6bb91b6f4f4cd88d2c53
meer-khan <shahmirkhan519@gmail.com>
2023-07-22 02:17:36 +0500

    added attributes given by viewset class

--- Restframework/ViewSet8/views.py
+++ Restframework/ViewSet8/views.py
@@ -8,11 +8,34 @@ from rest_framework.response import Response
 
 class StudentViewSet(viewsets.ViewSet):
     def list(self,request):
+      # Attributes given by viewsets
+      print("************LIST**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
+
       stu = Student.objects.all()
       serializer = StudentSerializer(stu,many=True)
       return Response(serializer.data)
     
     def retrieve(self,request, pk=None):
+
+
+      # Attributes given by viewsets
+      print("************RETRIEVE**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("REQUEST: ", self.request)
+      print("FORMAT KWARGS: ", self.format_kwarg)
+      print("KWARG: ", self.kwargs)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
+
       if pk is not None: 
          
         stu = Student.objects.get(pk=pk)
@@ -21,14 +44,38 @@ class StudentViewSet(viewsets.ViewSet):
       
 
     def create(self,request):
+            # Attributes given by viewsets
+      print("************CREATE**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
+
+
       serializer = StudentSerializer(data = request.data)
       if serializer.is_valid():
+        print(serializer.validated_data)
+        # name = serializer.validated_data['name']
+    
         serializer.save()
+        print(serializer.data)
         return Response(serializer.data, status= status.HTTP_201_CREATED)
 
       return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
     
     def update(self,request,pk):
+            # Attributes given by viewsets
+      print("************UPDATE**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
+
+
       # id = pk 
       if pk is not None:
         stu = Student.objects.get(pk=pk)
@@ -39,6 +86,14 @@ class StudentViewSet(viewsets.ViewSet):
         return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
       
     def partial_update(self,request,pk):
+            # Attributes given by viewsets
+      print("************PARTIAL UPDATE**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
       if pk is not None:
         stu = Student.objects.get(pk=pk)
         serializer = StudentSerializer(instance = stu, 
@@ -50,6 +105,14 @@ class StudentViewSet(viewsets.ViewSet):
       
 
     def destroy(self,request,pk):
+            # Attributes given by viewsets
+      print("************DESTROY**************")
+      print("Basename: ", self.basename)
+      print("Action: ", self.action)
+      print("Detail: ", self.detail)
+      print("Suffix: ", self.suffix)
+      print("Name: ", self.name)
+      print("Description: ", self.description)
       if pk is not None:
         stu = Student.objects.get(pk=pk)
         stu.delete()
