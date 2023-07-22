e7e297e5b2742e2ed0aed26e2ccbea322721db7a
meer-khan <shahmirkhan519@gmail.com>
2023-07-22 15:55:57 +0500

    created a new app for model viewset

---
+++ Restframework/ModelViewSet9/urls.py
@@ -0,0 +1,15 @@
+# from .views import *
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+# Create Router Object
+router = DefaultRouter()
+
+# Register StudentViewSet with Router
+router.register("studentapi", views.StudentViewSet, basename = 'student')
+urlpatterns = [
+    path("", include(router.urls))
+
+]
