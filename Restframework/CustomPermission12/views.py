0c35a5f37acafc1e137cc9e14b16d216c764f36d
meer-khan <shahmirkhan519@gmail.com>
2023-08-15 02:29:01 +0500

    custom permissions

--- Restframework/CustomPermission12/views.py
+++ Restframework/CustomPermission12/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import (IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, 
                                         DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly)
+from .custompermissions import MyPermission
 # Create your views here.
 
 
@@ -19,7 +20,11 @@ class StudentViewSet(viewsets.ModelViewSet):
   # permission_classes = [IsAdminUser] # this is only accessible by the user who are staff "is staff true"
   # permission_classes = [IsAuthenticatedOrReadOnly]
   # permission_classes = [DjangoModelPermissions]
-  permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+  # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+  # This is custome permission
+  permission_classes = [MyPermission]
+
 
 
 
