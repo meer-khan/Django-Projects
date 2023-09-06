a52847b192981ee117a1c922540f8a9ec351c30a
meer-khan <shahmirkhan519@gmail.com>
2023-09-06 17:11:12 +0500

    created routes for token generation, refreshing and verifying

--- Restframework/JWTAuthentication13/urls.py
+++ Restframework/JWTAuthentication13/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # Create Router Object
 router = DefaultRouter()
@@ -13,6 +13,10 @@ router.register("studentapi", views.StudentViewSet, basename = 'student')
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('gettoken/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('refreshtoken/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    path('verifytoken/', TokenVerifyView.as_view(), name = 'token_verify'),
+
 
     # To get a login button on browseable API
     path("auth/", include("rest_framework.urls"))
