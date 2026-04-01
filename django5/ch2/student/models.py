bdda745e27b2f73eae80bc5884cdd4ecea208472
meer-khan <shahmirkhan519@gmail.com>
2026-04-01 23:08:24 +0500

    added another model of results

--- django5/ch2/student/models.py
+++ django5/ch2/student/models.py
@@ -8,5 +8,16 @@ class Profile(models.Model):
     city = models.CharField(max_length=100)
     roll = models.CharField(max_length=20)
 
+
+    # This method is used to return a string representation of the object,
+    #  which is useful for debugging and displaying the object in the admin interface.
     def __str__(self):
         return self.name
+    
+class Result(models.Model):
+    student = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    marks = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student.name} - {self.subject}"
