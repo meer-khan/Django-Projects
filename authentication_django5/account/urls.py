0a5e6a79c33e1a64eaaa7242be2686c4bacf7c83
meer-khan <shahmirkhan519@gmail.com>
2026-05-30 03:08:16 +0500

    added urls

--- authentication_django5/account/urls.py
+++ authentication_django5/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import register
+from .views import register, login_user, profile
 
 urlpatterns = [
     path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("profile/", profile, name="profile"),
 ]
