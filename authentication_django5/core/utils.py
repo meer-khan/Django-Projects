f10100291f72efeaa6990572333429b58f416769
meer-khan <shahmirkhan519@gmail.com>
2026-06-10 03:18:25 +0500

    assign permissions function

--- authentication_django5/core/utils.py
+++ authentication_django5/core/utils.py
@@ -1,4 +1,20 @@
 import secrets
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from core.permission_config import PERMISSION_CONFIG
+from account.models import User
+
+def assign_permissions(user: User):
+    role_permissions = PERMISSION_CONFIG.get(user.role, {})
+    for model, perms in role_permissions.items():
+        content_type = ContentType.objects.get_for_model(model)
+        for perm_codename in perms:
+            permission_codename = f"{perm_codename}_{model._meta.model_name}"
+            try:
+                permission = Permission.objects.get(codename=permission_codename, content_type=content_type)
+                user.user_permissions.add(permission)
+            except Permission.DoesNotExist:
+                print(f"Permission {permission_codename} does not exist.")
 
 def generate_otp():
     return str(secrets.randbelow(900000) + 100000)
